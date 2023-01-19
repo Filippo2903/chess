94116d203661a36d4883d122caf3b6168bb9d9d8
Filippo <filippo.gentili.06@gmail.com>
2023-01-19 17:56:39 +0100

    commit

--- client/src/Main.java
+++ client/src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Sivalletto game = new Sivalletto();
+        game.initGame();
     }
 }
